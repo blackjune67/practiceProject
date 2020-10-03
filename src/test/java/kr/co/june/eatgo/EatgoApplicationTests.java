598ec82e3a9682bf53450a1dd1630a3b81af217d
blackjune67 <buea486@gmail.com>
2020-10-03 22:45:15 +0900

    init

---
+++ src/test/java/kr/co/june/eatgo/EatgoApplicationTests.java
@@ -0,0 +1,17 @@
+package kr.co.june.eatgo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EatgoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+//asdasdsd
+	}
+
+}
