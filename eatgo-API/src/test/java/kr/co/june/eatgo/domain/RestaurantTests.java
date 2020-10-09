85419f2a52519262bf433c495e63d878a3667fba
blackjune67 <buea486@gmail.com>
2020-10-09 17:15:54 +0900

    chapter1 TEST Driven Success

--- eatgo-API/src/test/java/kr/co/june/eatgo/domain/RestaurantTests.java
+++ eatgo-API/src/test/java/kr/co/june/eatgo/domain/RestaurantTests.java
@@ -1,13 +1,17 @@
 package kr.co.june.eatgo.domain;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
-//import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class RestaurantTests {
+
+    /*
+     *  테스트 코드 작성시 assertThat으로 테스트를 사용할 수 있으며,
+     * is를 사용하영 객체의 파라미터와 자신의 파라미터가 동일한지 테스트를 진행할 수 있다.
+     *
+     */
 
     @Test
     public void craetion() throws Exception {
@@ -22,9 +26,4 @@ public class RestaurantTests {
         Restaurant RestaurantInformation = new Restaurant("Bob zip", "Seoul");
         assertThat(RestaurantInformation.getInformation(), is("Bob zip in Seoul"));
     }
-
-    /*
-    *  테스트 코드 작성시 assertThat으로 테스트를 사용할 수 있으며,
-    * is를 사용하영 객체의 파라미터와 자신의 파라미터가 동일한지 테스트를 진행할 수 있다.
-    * */
 }
